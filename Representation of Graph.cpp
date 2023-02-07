777952f614fa71fc7a4f1ed65f0cc59404f74fbc
Eshaan Pandey <84571751+eshaan446@users.noreply.github.com>
2023-02-07 23:08:24 +0530

    Update Representation of Graph.cpp

--- Representation of Graph.cpp
+++ Representation of Graph.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 //Representation of Graph in C++
 int main(){ 
-int n,m;
+int n,m; //n=number of node/vertex , m=number of edges
 unordered_map<int,list<int>>adj;
 cin>>n>>m;
+//Now take those each edge as input (take it singularly no need to double write it(it should be one time)
 for(int i=1;i<=m;i++){ //1 because the graph was one-based, else it starts with zero
     int u,v;
     cin>>u>>v;
     adj[u].push_back(v);
+    //depends whether it is a directed graph or undirected graph
     adj[v].push_back(u);
 }
 //Now our graph is stored/represented in the adjacency list.
